ac7d2396d848e506a6d65f442de457923871cd09
bokc-choy <134495956+bokc-choy@users.noreply.github.com>
2024-01-10 17:04:51 -0500

    Added grid boxes with mouse interactive event listeners

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ const gridContainer = document.querySelector(".gridContainer");
 for(let i = 0; i < 16; i++){
     const grid = document.createElement("div");
     grid.className = "row";
-    grid.textContent = "r";
     for(let j = 0; j < 16; j++){
         const subGrid = document.createElement("div");
         subGrid.className = "column";
-        subGrid.textContent = "r";
+        subGrid.addEventListener('mouseover', function(){
+            subGrid.style.backgroundColor = "red";
+        })
+        subGrid.addEventListener('mouseout', function(){
+            subGrid.style.backgroundColor = "grey";
+        })
         grid.append(subGrid);
     }
     gridContainer.append(grid);
 }
+
+
 
 
 
