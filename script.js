35b55ab1bbcd8e36d49029f749218a87ee4cf204
bokc-choy <134495956+bokc-choy@users.noreply.github.com>
2024-01-11 15:14:56 -0500

    Made button to create grid at desired size
    Work on resizing grid after it is made

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const gridContainer = document.querySelector(".gridContainer");
+const resizebtn = document.querySelector(".resizebtn")
 
-for(let i = 0; i < 16; i++){
-    const grid = document.createElement("div");
-    grid.className = "row";
-    for(let j = 0; j < 16; j++){
-        const subGrid = document.createElement("div");
-        subGrid.className = "column";
-        subGrid.addEventListener('mouseover', function(){
-            subGrid.style.backgroundColor = "red";
-        })
-        subGrid.addEventListener('mouseout', function(){
-            subGrid.style.backgroundColor = "grey";
-        })
-        grid.append(subGrid);
+function createGrid(size){
+    for(let i = 0; i < size; i++){
+        const grid = document.createElement("div");
+        grid.className = "row";
+        for(let j = 0; j < size; j++){
+            const subGrid = document.createElement("div");
+            subGrid.className = "column";
+            subGrid.addEventListener('mouseover', function(){
+                subGrid.style.backgroundColor = "red";
+            })
+            subGrid.addEventListener('mouseout', function(){
+                subGrid.style.backgroundColor = "grey";
+            })
+            grid.append(subGrid);
+        }
+        gridContainer.append(grid);
     }
-    gridContainer.append(grid);
 }
 
-
+resizebtn.addEventListener('click', function(){
+    let size = prompt('enter size')
+    createGrid(size);
+})
 
 
 
