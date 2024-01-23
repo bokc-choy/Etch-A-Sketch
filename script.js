06f52269e02153b31807c721896db43d9d086812
bokc-choy <134495956+bokc-choy@users.noreply.github.com>
2024-01-22 21:36:12 -0500

    Moved buttons from the top to the left Added clear button to clear current grid Changed sketch function to work on mouse click/drag

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const gridContainer = document.querySelector(".gridContainer");
 const resizebtn = document.querySelector(".resizebtn");
+const clearbtn = document.querySelector(".clearbtn");
+let size;
+
+let mouseDown = false;
+document.addEventListener('mousedown', function(){mouseDown = true;
+console.log(mouseDown)})
+document.addEventListener('mouseup', function(){mouseDown = false;
+console.log(mouseDown)})
 
 function createGrid(size){
     for(let i = 0; i < size; i++){
@@ -11,9 +19,16 @@ function createGrid(size){
             subGrid.style.width = (1/size)*650 + 'px';
             subGrid.style.height = (1/size)*650 + 'px';
             subGrid.style.borderWidth = (1/size)*2 + 'px';
+            subGrid.addEventListener('mousedown', function(){
+                    console.log("color");
+                    subGrid.style.backgroundColor = "black";
+            });
             subGrid.addEventListener('mouseover', function(){
-                subGrid.style.backgroundColor = "black";
-            })
+                if(mouseDown == true){
+                    console.log("color");
+                    subGrid.style.backgroundColor = "black";
+                }
+            });
             grid.append(subGrid);
         }
         gridContainer.append(grid);
@@ -27,18 +42,22 @@ function removeGrid(){
         child = gridContainer.lastElementChild;
 
     }
-
 }
 
 resizebtn.addEventListener('click', function(){
     removeGrid();
-    let size = prompt('enter grid size (max 100)')
+    size = prompt('enter grid size (max 100)')
     if(size >= 100){
 
     }
     else{
     createGrid(size);
     }
+})
+
+clearbtn.addEventListener('click', function(){
+    removeGrid();
+    createGrid(size);
 })
 
 
